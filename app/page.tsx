9908cf39f4c286c003b4b0d3abfc949b60e8410f
IrinaB <balkarovairina@gmail.com>
2024-11-02 13:07:44 +0100

    ESLint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-      <h1 className='text-5xl'>Hello</h1>
-  );
+    return <h1 className="text-5xl font-light">Hello</h1>
 }
