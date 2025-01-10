8d0299999c184708bf2bb8ac35b1cca3ed922762
IrinaB <balkarovairina@gmail.com>
2025-01-10 12:54:54 +0100

    Add Initial ShadCN UI Setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
     return (
-        <h1 className='text-3xl text-violet-600'>Welcome to the world of Next.js</h1>
+        <>
+            <h1 className='text-3xl text-violet-600'>Welcome to the world of Next.js</h1>
+
+        </>
     )
 }
