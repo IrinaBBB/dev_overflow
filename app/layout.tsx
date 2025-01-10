92372c80b212232ed0c86b2d74758ef91bc5d996
IrinaB <balkarovairina@gmail.com>
2025-01-10 15:43:12 +0100

    Add Top Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 
 import './globals.css'
 import { ThemeProvider } from 'next-themes'
+import Navbar from '@/components/navigation/navbar'
 
 const inter = localFont({
     src: './fonts/InterVF.ttf',
@@ -38,6 +39,7 @@ export default function RootLayout({
             className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
         >
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
+            <Navbar />
             {children}
         </ThemeProvider>
         </body>
---
+++ components/navigation/navbar/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+function Navbar(props) {
+    return (
+        <nav
+            className='flex-between background-light900_dark200 fixed z-50 w-full p-6 shadow-light-300 dark:shadow-none sm:px-12 gap-5'>
+            <Link href='/' className='flex items-center gap-1'>
+                <Image src='/images/site-logo.svg' alt='DevOverlow Logo' width={23} height={23} />
+                <p className='h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden'>
+                    Dev <span className='text-primary-500'>Overflow</span>
+                </p>
+            </Link>
+            <p>Global Search</p>
+            <div className='flex-between gap-5'>
+                Theme Picker
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
