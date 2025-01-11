9dd37b6a0b933cabb23af557736e102d154a2df7
IrinaB <balkarovairina@gmail.com>
2025-01-11 15:28:15 +0100

    Add Auth Form UI with Client Side Validation

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,18 @@
+'use client'
+
 import React from 'react'
 
-function SignIn(props) {
+import AuthForm from '@/components/forms/AuthForm'
+import { SignInSchema } from '@/lib/validations'
+
+const SignIn = () => {
     return (
-        <div>
-            Sign In
-        </div>
+        <AuthForm
+            formType='SIGN_IN'
+            schema={SignInSchema}
+            defaultValues={{ email: '', password: '' }}
+            onSubmit={(data) => Promise.resolve({ success: true, data })}
+        />
     )
 }
 
--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -46,16 +46,16 @@ const SocialAuthForm = () => {
                 <span>Log in with GitHub</span>
             </Button>
 
-            <Button className={buttonClass} onClick={() => handleSignIn('google')}>
-                <Image
-                    src='/icons/google.svg'
-                    alt='Google Logo'
-                    width={20}
-                    height={20}
-                    className='mr-2.5 object-contain'
-                />
-                <span>Log in with Google</span>
-            </Button>
+            {/*<Button className={buttonClass} onClick={() => handleSignIn('google')}>*/}
+            {/*    <Image*/}
+            {/*        src='/icons/google.svg'*/}
+            {/*        alt='Google Logo'*/}
+            {/*        width={20}*/}
+            {/*        height={20}*/}
+            {/*        className='mr-2.5 object-contain'*/}
+            {/*    />*/}
+            {/*    <span>Log in with Google</span>*/}
+            {/*</Button>*/}
         </div>
     )
 }
